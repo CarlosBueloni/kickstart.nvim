83780ba70d214360e8c93c2cee316c9d1f18fe0e
CarlosBueloni <carlosbueloni@gmail.com>
2025-05-28 17:11:13 -0300

    change settings

--- lua/custom/settings.lua
+++ lua/custom/settings.lua
@@ -1,8 +1,8 @@
 -- File: ~/.config/nvim/lua/custom/settings.lua
 
 -- Global tab settings
-vim.opt.tabstop = 2 -- Number of spaces a tab counts for
-vim.opt.softtabstop = 2 -- Number of spaces inserted for a tab
+vim.opt.tabstop = 4 -- Number of spaces a tab counts for
+vim.opt.softtabstop = 4 -- Number of spaces inserted for a tab
 vim.opt.shiftwidth = 2 -- Number of spaces for each step of indent
 vim.opt.expandtab = true -- Convert tabs to spaces
 
